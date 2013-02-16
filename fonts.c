4c06c19273faa3c40a4cd8f5918cc1973ca8a309
Christian Vogel <vogelchr@vogel.cx>
2013-02-16 17:14:20 +0100

    fonts: major rewrite, very ugly.

--- fonts.c
+++ fonts.c
@@ -1,11 +1,151 @@
 #include "fonts.h"
 
-static PROGMEM const struct font_info * const fonts[1] = {
-	& font6x8_info
+#include <stdio.h>
+
+static PROGMEM const struct font_info * const fonts[] = {
+	& font6x8_info,
+	& font4x5_info
 };
 
-void
-fonts_put_char(int fontnum,char c,uint8_t *fb,int x, int y){
-	const PROGMEM struct font_info *fi = fonts[fontnum];
+#ifdef DEBUG_FONT_DATA_STRUCTURES
+#define DEBUG(...) printf_P(__VA_ARGS)
+#else
+#define DEBUG /* ... */
+#endif
 
+uint8_t
+font_puts_generic(
+	enum font fontnum,
+	const char *s,
+	uint8_t *fb,
+	int x,
+	int xspace,
+	int y,
+	enum font_stamp_mode mode,
+	int in_progmem
+){
+	char c;
+	uint8_t ret;
+	uint8_t sum = 0;
+
+	while(xspace > 0){
+		if (in_progmem)
+			c = pgm_read_byte(s++);
+		else
+			c = *s++;
+
+		if(!c)
+			break;
+		ret = fonts_put_char(fontnum, c, fb, x, xspace, y, mode);
+		sum += ret;
+		x += ret;
+		xspace -= ret;
+
+		if(xspace){
+			x++;
+			xspace--;
+			sum++;
+		}
+	}
+	return sum;
 }
+
+
+uint8_t
+font_puts_RAM(
+	enum font fontnum,
+	const char *s,
+	uint8_t *fb,
+	int x,
+	int xspace,
+	int y,
+	enum font_stamp_mode mode
+){
+	return font_puts_generic(fontnum,s,fb,x,xspace,y,mode,0);
+}
+
+uint8_t
+font_puts_P(
+	enum font fontnum,
+	const char *s,
+	uint8_t *fb,
+	int x,
+	int xspace,
+	int y,
+	enum font_stamp_mode mode
+){
+	return font_puts_generic(fontnum,s,fb,x,xspace,y,mode,1);
+}
+
+
+uint8_t
+fonts_put_char(
+	enum font fontnum,
+	char c,
+	uint8_t *fb,
+	int x,
+	int xspace,
+	int y,
+	enum font_stamp_mode mode
+){
+	const struct font_info *fip;
+	const struct font_offset *offs;
+	const uint8_t *data;
+
+	uint8_t width,height,code, mask, bits;
+	uint16_t offset_in_data;
+	int i;
+
+
+	fip = (const struct font_info*) pgm_read_word(fonts+fontnum);
+	offs = (const struct font_offset*) pgm_read_word(&(fip->offs));
+	data = (const uint8_t *)pgm_read_word(&(fip->data));
+
+	DEBUG(PSTR("Font %d based on fip=%p, offs=%p, data=%p\n"),
+		fontnum,fip,offs,data);
+
+	width = pgm_read_byte(&(fip->width));
+	height = pgm_read_byte(&(fip->height));
+
+	do {
+		code = pgm_read_byte(&(offs->code));
+		offset_in_data = pgm_read_word(&(offs->offs));
+		if (code == c )
+			break;
+		offs++;
+	} while (code != 0xff); /* end */
+
+	if (code == 0xff) {
+		DEBUG(PSTR("  Char 0x%02x not found.\n"),c);
+		return 0;
+	}
+
+	DEBUG(PSTR("  Char 0x%02x, offset info @ %p, offs = 0x%02x\n"),
+		code,offs,offset_in_data);
+
+	/* seek to requested character */
+	data += offset_in_data;
+	fb += x;
+	mask = ((1<<height)-1) << y;
+
+
+	for (i=0;i<width;i++) {
+		bits = pgm_read_byte(data);
+		bits <<= y;
+
+		if (mode == FONT_STAMP_INV)
+			bits = ~bits;
+
+		data++;
+		DEBUG(PSTR("Character 0x%02x, row %d, data @ %p = 0x%02x.\n"),
+			c, i, data, bits);
+		*fb = (*fb & ~mask)|(bits & mask);
+		fb++;
+
+		if(i >= xspace)
+			break;
+	}
+
+	return i;
+}
+
--- fonts.h
+++ fonts.h
@@ -4,6 +4,16 @@
 #include <stddef.h>
 #include <avr/pgmspace.h>
 
+enum font {
+	FONT6X8,
+	FONT4X5
+};
+
+enum font_stamp_mode {
+	FONT_STAMP_NORM,  /* replace character area */
+	FONT_STAMP_INV    /* replace character area with inverse */
+};
+
 struct font_info {
 	uint8_t width,height,nchars;
 	const uint8_t * data;
@@ -16,6 +26,20 @@ struct font_offset {
 	uint16_t offs;
 };
 
+extern uint8_t font_puts_RAM(
+	enum font fontnum, const char *s, uint8_t *fb, int x,
+	int xspace, int y, enum font_stamp_mode mode );
+
+extern uint8_t font_puts_P(
+	enum font fontnum, const char *s, uint8_t *fb, int x,
+	int xspace, int y, enum font_stamp_mode mode );
+
+extern uint8_t fonts_put_char(
+	enum font fontnum, char c, uint8_t *fb, int x,
+	int xspace, int y, enum font_stamp_mode mode );
+
+
 extern const PROGMEM struct font_info font6x8_info;
+extern const PROGMEM struct font_info font4x5_info;
 
 #endif
